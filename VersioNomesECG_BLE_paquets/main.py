01ef7461d68c1cac7b0bb11597042ce6602dc7cc
arnau-K <deprezsantamaria1@gmail.com>
2025-04-08 17:59:26 +0200

    més amplada perquè no surtin ralles

--- VersioNomesECG_BLE_paquets/main.py
+++ VersioNomesECG_BLE_paquets/main.py
@@ -5,6 +5,6 @@ from gui import MainWindow
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
-    window.resize(1280, 720)
+    window.resize(1280, 800)
     window.show()
     sys.exit(app.exec())
